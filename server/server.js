27b3c500b69d7115c20be5d1480fa639cf875694
sramk87 <sramkissoon87@gmail.com>
2022-12-01 00:19:09 -0500

    server and middleware

--- server/server.js
+++ server/server.js
@@ -2,12 +2,27 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/auth");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+async function wait (){
+  await server.start()
+  server.applyMiddleware({ app });;
+}
+wait()
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
